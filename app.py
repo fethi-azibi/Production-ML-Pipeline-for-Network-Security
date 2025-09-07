b7de633cab2b8bcff96aedc86cb1d3e03c533470
fethi-azibi <azibi.fethi99@gmail.com>
2025-09-08 00:10:22 +0100

    making changes

--- app.py
+++ app.py
@@ -90,5 +90,7 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
             raise NetworkSecurityException(e,sys)
 
     
+
 if __name__=="__main__":
+    #to run server as python file
     app_run(app,host="0.0.0.0",port=8000)
